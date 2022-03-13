9b6a92a790fd2ae68bc1f24c067c7c4642f49acb
rossyvee <roseakinyi001@gmail.com>
2022-03-13 22:36:13 +0300

    added contact us form and social media section

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+function saveMessage() {
+    console.log("form is submitting")
+    let name = $("input#name").val()
+    let email = $("input#email").val()
+    // let message = $("textarea#message").val()
+    let message = $("textarea.message").val()
+
+    // const data = { name:name, email:email, message:"any value"}
+    // if key name is the same as the name storing the value
+    const data = { name:name , email:email , message:message }
+
+    // Reset form after sending the message
+    $("form#message-form")[0].reset()
+
+    console.log(data)
+
+    $("div#response-message").show()
+}
+$(document).ready(function () {
+
+})
